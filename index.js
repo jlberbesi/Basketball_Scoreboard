b5a2d774cdc550a23927b7bf75e5ff76f8b30041
Jlberbesi <jlberbesi.walloom@gmail.com>
2023-04-14 19:13:22 -0500

    Add corrections in index.js file

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const homeCountEl = document.getElementById('count-elis');
 let count = 0;
 let homeCount = 0;
 
+// eslint-disable-next-line no-unused-vars
 function incrementar(cantidad, esHome) {
   if (cantidad >= 1 && cantidad <= 3) {
     if (esHome) {
@@ -15,27 +16,3 @@ function incrementar(cantidad, esHome) {
     }
   }
 }
-
-document.getElementById('button-1').addEventListener('click', () => {
-  incrementar(1, false);
-});
-
-document.getElementById('button-2').addEventListener('click', () => {
-  incrementar(2, false);
-});
-
-document.getElementById('button-3').addEventListener('click', () => {
-  incrementar(3, false);
-});
-
-document.getElementById('home-button-1').addEventListener('click', () => {
-  incrementar(1, true);
-});
-
-document.getElementById('home-button-2').addEventListener('click', () => {
-  incrementar(2, true);
-});
-
-document.getElementById('home-button-3').addEventListener('click', () => {
-  incrementar(3, true);
-});
